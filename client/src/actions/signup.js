53d0789bb9263ab620e6a694c48f4e6e0cf09d97
adambelton <thelastgreatcynic@gmail.com>
2018-06-01 15:27:38 +0200

    final final commit before demo

--- client/src/actions/signup.js
+++ client/src/actions/signup.js
@@ -6,27 +6,6 @@ export const USER_SIGNUP_FAILED = 'USER_SIGNUP_FAILED'
 export const USER_REGISTER_SUCCESS = 'USER_REGISTER_SUCCESS'
 export const USER_REGISTER_FAILED = 'USER_REGISTER_FAILED'
 
-// export const signup = (firstName, lastName, email, password) => (dispatch) =>
-// 	request
-// 		.post(`${BASE_URL}/users`)
-// 		.send({ firstName: firstName, lastName: lastName, email, password })
-// 		.then(result => {
-// 			dispatch({
-// 				type: USER_SIGNUP_SUCCESS
-// 			})
-// 		})
-// 		.catch(err => {
-// 			if (err.status === 400) {
-// 				dispatch({
-// 					type: USER_SIGNUP_FAILED,
-// 					payload: err.response.body.message || 'Unknown error'
-// 				})
-// 			}
-// 			else {
-// 				console.error(err)
-// 			}
-// 		})
-
 export const register = (
   firstName,
   lastName,
